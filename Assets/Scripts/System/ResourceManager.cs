a4dfbaab86f4869ec65e3a17453a69b8cfbf73e1
Ellug <auron975@nate.com>
2025-11-24 18:10:14 +0900

    [Feat] Resource UI
    - Resource Manager -> Resource UI Update

--- Assets/Scripts/System/ResourceManager.cs
+++ Assets/Scripts/System/ResourceManager.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 
 public class ResourceManager : MonoBehaviour
@@ -5,9 +6,13 @@ public class ResourceManager : MonoBehaviour
     public static ResourceManager Instance { get; private set; }
 
     [SerializeField] private int _wood = 0;
+    [SerializeField] private int _food = 0;
+    [SerializeField] private TMP_Text _woodCountText;
+    [SerializeField] private TMP_Text _foodCountText;
 
     // Properties
     public int CurrentWood => _wood;
+    public int CurrentFood => _food;
 
     void Awake()
     {
@@ -22,9 +27,15 @@ public class ResourceManager : MonoBehaviour
         }
     }
 
+    void Start()
+    {
+        RefreshUI();
+    }
+
     public void AddWood(int amout)
     {
         _wood += amout;
+        RefreshUI();
     }
 
     public bool TryConsume( int amout)
@@ -32,6 +43,16 @@ public class ResourceManager : MonoBehaviour
         if (_wood < amout) return false;
 
         _wood -= amout;
+        RefreshUI();
         return true;
+    }
+
+    private void RefreshUI()
+    {
+        if (_woodCountText != null)
+            _woodCountText.text = _wood.ToString();
+
+        if (_foodCountText != null)
+            _foodCountText.text = _food.ToString();
     }
 }
